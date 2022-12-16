7e5e33ff4892843d0b3bfa7401b53d66041d8bc1
anthonycepeda <cepedanthony@gmail.com>
2022-12-16 18:13:42 +0100

    fix import error

--- api/models/generic.py
+++ api/models/generic.py
@@ -1,5 +1,4 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, EmailStr
 
 class Error(BaseModel):
     """Erros reponses Model"""
@@ -12,5 +11,31 @@ class Error(BaseModel):
             "example": {
                 "detail": "MongoDb not ready",
                 "status_code": 406,
+            }
+        }
+
+
+
+
+
+class User(BaseModel):
+    """User Model"""
+
+    user_id: str
+    username: str
+    name: str
+    email: EmailStr
+    active: bool
+
+    class Config:
+        """Swagger example"""
+
+        schema_extra = {
+            "example": {
+                "user_id": "a00001",
+                "username": "anth0",
+                "name": "Anthony Cepeda",
+                "email": "anth@mail.com",
+                "active": True,
             }
         }
